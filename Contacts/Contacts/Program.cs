b5d23ae34fdccdfa74ef2d40e1151ea089737bb1
Larysa Hradyska <l.v.hradyska@gmail.com>
2023-03-14 11:38:18 +0200

    adding empty project

---
+++ Contacts/Contacts/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Contacts
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
